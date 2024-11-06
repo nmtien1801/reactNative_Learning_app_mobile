1fafa7d69bb40936bf06c318aafbd26adde5a77a
nmtien1801 <nickname1801@gmail.com>
2024-11-06 22:04:34 +0700

    UPDATE: drawer in header + move file to folder user

--- src/header/Header-Course-Detail.js
+++ src/header/Header-Course-Detail.js
@@ -6,54 +6,62 @@ import {
   View,
   TouchableOpacity,
   CheckBox,
+  Image
 } from "react-native";
 import React, { useState } from "react";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
-const HeaderCourseDetail = ({ navigation , route}) => {
-  
+const HeaderCourseDetail = ({ navigation, route }) => {
+
   return (
     <View style={styles.header}>
-          <TouchableOpacity style={styles.backButton} onPress={()=> navigation.goBack()}>
-            <Ionicons name="chevron-back" size={24} color="black" />
-          </TouchableOpacity>
-          <Text style={styles.titleBar}>Course details</Text>
-          <View style={styles.rightIcons}>
-            <TouchableOpacity style={styles.iconButton}>
-              <Ionicons name="bookmark-outline" size={24} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.iconButton}>
-              <Ionicons name="ellipsis-vertical" size={24} color="black" />
-            </TouchableOpacity>
-          </View>
-        </View>
+    <TouchableOpacity
+      style={styles.backButton}
+      onPress={() => navigation.goBack()}
+    >
+      <Ionicons name="chevron-back" size={24} color="black" />
+    </TouchableOpacity>
+    <Text style={styles.titleBar}>Course details</Text>
+    <View style={styles.rightIcons}>
+      <TouchableOpacity style={styles.iconButton}>
+        <Ionicons name="bookmark-outline" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.iconButton}
+        onPress={() => navigation.openDrawer()}
+      >
+        <Ionicons name="ellipsis-vertical" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 16,
-        paddingVertical: 12,
-        borderBottomWidth: 1,
-        borderBottomColor: '#E0E0E0',
-      },
-      backButton: {
-        padding: 4,
-      },
-      titleBar: {
-        fontSize: 18,
-        fontWeight: 'bold',
-      },
-      rightIcons: {
-        flexDirection: 'row',
-      },
-      iconButton: {
-        padding: 4,
-        marginLeft: 16,
-      },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E0E0E0",
+  },
+
+  backButton: {
+    padding: 4,
+  },
+  titleBar: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  rightIcons: {
+    flexDirection: "row",
+  },
+  iconButton: {
+    padding: 4,
+    marginLeft: 16,
+  },
 });
 
 export default HeaderCourseDetail;
