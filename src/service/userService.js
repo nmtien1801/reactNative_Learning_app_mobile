7432dbd1edd1f6700848ddbffbe57cc73a37ef5a
nmtien1801 <nickname1801@gmail.com>
2024-11-17 00:31:25 +0700

    config .env cho reactnative

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { Platform } from "react-native";
+import {REACT_APP_BACKEND_ANDROID_URL, REACT_APP_BACKEND_WEB_URL} from '@env'
 
 const baseUrl =
-  Platform.OS === "android" || Platform.OS === "web"
-    ? "http://192.168.1.4:8080/api" // URL cho Android và iOS
-    : "http://localhost:8080/api"; // URL cho web hoặc môi trường khác
+  Platform.OS === "android"
+    ? REACT_APP_BACKEND_ANDROID_URL // URL cho Android và iOS
+    : REACT_APP_BACKEND_WEB_URL; // URL cho web hoặc môi trường khác
 
 const handleLoginApi = (email, password) => {
   return axios.post(`${baseUrl}/login`, { email, password });
