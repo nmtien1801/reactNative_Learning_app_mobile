05e89832bc463cd8b93fc0104c23ebeaedfe792c
nmtien1801 <nickname1801@gmail.com>
2024-11-20 11:31:56 +0700

    update: call api getAllCourse -> recomment course in home

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './authSlice'
+import courseReducer from './courseSlide'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    course: courseReducer
   },
 })
