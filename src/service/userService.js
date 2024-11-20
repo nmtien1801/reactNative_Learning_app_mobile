3fa9adad1bb62c4cefe280fb4c006929282b2f00
nmtien1801 <nickname1801@gmail.com>
2024-11-20 09:27:11 +0700

    reset git

--- src/service/userService.js
+++ src/service/userService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { Platform } from "react-native";
 
 const baseUrl =
-  Platform.OS === "android" || Platform.OS === "web"
-    ? "http://192.168.1.4:8080/api" // URL cho Android và iOS
+  Platform.OS === "android" 
+    ? "http://192.168.1.6:8080/api" // URL cho Android và iOS
     : "http://localhost:8080/api"; // URL cho web hoặc môi trường khác
 
 const handleLoginApi = (email, password) => {
